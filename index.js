d6ccb55119a70051335adc958c359a879e10a820
abenzoar yannis <abenzoaryannis.dev@gmail.com>
2022-01-25 20:57:22 +0100

    logique de check déclaré

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ const inputs = document.querySelectorAll(
   'input[type="text"], input[type="password"]'
 );
 
+const pseudoChecker = () => {};
+
+const emailChecker = () => {};
+
+const passwordChecker = () => {};
+
+const confirmChecker = () => {};
+
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
-    console.log(e.target.id);
+    switch (e.target.id) {
+      case "pseudo":
+        pseudoChecker();
+        break;
+      case "email":
+        emailChecker();
+        break;
+      case "password":
+        pseudoChecker();
+        break;
+      case "confirm":
+        confirmChecker();
+        break;
+      default:
+        null;
+    }
   });
 });
